7915b03a5ce890e091533f11d1a798d1e633db49
Justin Donaldson <jdonaldson@gmail.com>
2017-02-09 14:13:21 -0800

    inline abstract types, fix type of get_length

--- src/main/sys/db/Postgres.hx
+++ src/main/sys/db/Postgres.hx
@@ -330,7 +330,7 @@ class PostgresResultSet implements ResultSet {
 	var cached_rows        : Array<Array<Bytes>>;
 
 	var row_count                  = 0;
-	var set_length  : Null<Int>    = null;
+	var set_length  : Int    = 0;
 	var current_row : Array<Bytes> = null;
 
 	public var length(get, null)  : Int;
